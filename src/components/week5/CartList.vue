668aff93bc6058a4a5981453869068dc624f0463
runweiting <whatisfuntime@gmail.com>
2024-02-06 12:56:39 +0800

    [fix]: delete 和 spinner

--- src/components/week5/CartList.vue
+++ src/components/week5/CartList.vue
@@ -40,18 +40,23 @@ export default {
         </thead>
         <tbody>
           <tr v-for="item in carts" :key="item.id">
-            <td style="width: 160px">
+            <td style="width: 120px">
               <img :src="item.product.imageUrl" class="rounded img-fluid " style="height: 50px">
             </td>
-            <td style="width: 100px">
-              <div class="d-flex align-items-center">
+            <td style="width: 120px" class="pe-0">
+              <div class="d-flex justify-content-between align-items-center">
                 <div class="input-group input-group-sm" style="width: 80px;">
                   <!-- 加入 :disabled 避免重複觸發 -->
                   <input :disable="item.id === updateQty" @change="putQty(item)" v-model="item.qty" type="number" min="1" class="form-control">
                   <span class="input-group-text">{{ item.product.unit }}</span>
                 </div>
-                <div class="m-0 border-0 ms-2" style="width: 20px;">
-                  <span v-if="item.id === updateQty" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                <div class="m-0 border-0 d-flex" style="width: 40px;">
+                  <button :class="{ 'd-none': item.qty > 1}" @click="deleteItem(item.id)" type="button" class="btn btn-outline-danger" style="scale: 60%;">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                      <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                    </svg>
+                  </button>
+                  <span :class="{ 'd-none': item.qty === 1 }" v-if="item.id === updateQty" class="spinner-border spinner-border-sm ms-2" role="status" aria-hidden="true"></span>
                 </div>
               </div>
             </td>
