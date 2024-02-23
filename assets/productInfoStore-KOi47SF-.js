cd85281c8e271e021dd1ec572334e44df41b46cb
runweiting <whatisfuntime@gmail.com>
2024-02-23 16:15:29 +0800

    update 2024-02-23 16:15:29

---
+++ assets/productInfoStore-KOi47SF-.js
@@ -0,0 +1,1 @@
+import{s as e}from"./index-eNBx6K3w.js";const r=e("productInfoStore",{state:()=>({selectedProduct:null}),actions:{setSelectedProduct(t){this.selectedProduct=t}}});export{r as p};
--- assets/productInfoStore-KRLHb93V.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./index-pcAwSSA1.js";const r=e("productInfoStore",{state:()=>({selectedProduct:null}),actions:{setSelectedProduct(t){this.selectedProduct=t}}});export{r as p};
