b8cf18f6784e2f4780d3dcdb027071db2705f969
runweiting <whatisfuntime@gmail.com>
2024-03-07 20:48:33 +0800

    [feat]: NotFound.vue 設置回首頁按鈕

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="container">
-        <h1 class="pt-5 text-center">404 沒有這一頁喔～</h1>
+        <div class="row">
+            <div class="col">
+                <div class="text-center">
+                    <h1 class="pt-5 text-center">404 沒有這一頁喔～</h1>
+                    <RouterLink :to="{ name: 'home' }" class="btn btn-primary text-decoration-none">{{ $t('menu.home') }}</RouterLink>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
